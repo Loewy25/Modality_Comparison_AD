09d20fc60bda7bb0fa9ef0d9be4b36f41eb7068f
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2023-10-18 15:11:18 -0500

    Update cd.py

--- cd.py
+++ cd.py
@@ -9,7 +9,7 @@ image_pet,label,masker=loading_mask('cd','PET')
 
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'MRI_MAIN', 'cd')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'MRI', 'cd')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
@@ -20,7 +20,7 @@ print(f"The function took {elapsed_time:.2f} seconds to run.")
 
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'MRI_MAIN', 'cd')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cd')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
