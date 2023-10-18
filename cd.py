c1f9bbd39ff9fb167644f733830c1e0725cd7323
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2023-10-18 14:04:34 -0500

    Update cd.py

--- cd.py
+++ cd.py
@@ -17,7 +17,6 @@ print(f"The function took {elapsed_time:.2f} seconds to run.")
 
 
 
-//
 start_time = time.time()  # Capture start time
 
 performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'MRI_MAIN', 'cd')
