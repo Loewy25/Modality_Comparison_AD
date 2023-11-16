ff20150f88d5765496096071c83c790a3d927422
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2023-11-16 00:38:11 -0600

    Update cd.py

--- cd.py
+++ cd.py
@@ -15,6 +15,18 @@ end_time = time.time()  # Capture end time
 elapsed_time = end_time - start_time  # Calculate elapsed time
 print(f"The function took {elapsed_time:.2f} seconds to run.")
 
+image_mri,label,masker=loading_mask('cd','PET')
+#image_pet,label,masker=loading_mask('cd','PET')
+
+start_time = time.time()  # Capture start time
+
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'PET', 'cd')
+end_time = time.time()  # Capture end time
+
+elapsed_time = end_time - start_time  # Calculate elapsed time
+print(f"The function took {elapsed_time:.2f} seconds to run.")
+
+
 
 
 
