cecc9d446ad4a9e3c80a6af7d02400585916b6c6
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2023-11-28 18:09:03 -0600

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -2,6 +2,46 @@ from tensorflow.keras.layers import Conv3D, Input, LeakyReLU, Add, GlobalAverage
 from tensorflow.keras.models import Model
 from tensorflow.keras.regularizers import l2
 import tensorflow_addons as tfa
+import numpy as np
+from nilearn.image import resample_img, new_img_like
+
+
+def pad_image_to_shape(image, target_shape=(128, 128, 128)):
+    # Calculate the padding required in each dimension
+    padding = [(0, max(target_shape[dim] - image.shape[dim], 0)) for dim in range(3)]
+    
+    # Apply zero-padding to the data
+    new_data = np.pad(image.get_fdata(), padding, mode='constant', constant_values=0)
+    
+    # Adjust the affine to account for the new shape
+    new_affine = np.copy(image.affine)
+    
+    # Create and return a new NIfTI-like image with the padded data
+    return new_img_like(image, new_data, affine=new_affine)
+
+
+
+def resize(image, new_shape=(128, 128, 128), interpolation="linear"):
+    # Reorder the image and resample it with the desired interpolation
+    image = reorder_img(image, resample=interpolation)
+    
+    # Calculate the zoom levels needed for the new shape
+    zoom_level = np.divide(new_shape, image.shape[:3])
+    
+    # Calculate the new spacing for the image
+    new_spacing = np.divide(image.header.get_zooms()[:3], zoom_level)
+    
+    # Resample the image data to the new spacing
+    new_data = resample_to_spacing(image.get_fdata(), image.header.get_zooms()[:3], new_spacing, 
+                                   interpolation=interpolation)
+    # Copy and adjust the affine transformation matrix for the new spacing
+    new_affine = np.copy(image.affine)
+    np.fill_diagonal(new_affine, new_spacing.tolist() + [1])
+    new_affine[:3, 3] += calculate_origin_offset(new_spacing, image.header.get_zooms()[:3])
+    
+    # Create and return a new NIfTI-like image
+    return new_img_like(image, new_data, affine=new_affine)
+
 
 
 def convolution_block(x, filters, kernel_size=(3,3,3), strides=(1,1,1)):
