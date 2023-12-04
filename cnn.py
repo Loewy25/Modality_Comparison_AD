1c098d7f55449bf19d3ccabf2ee3ac1fa6eb8652
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2023-12-04 11:13:05 -0600

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -72,7 +72,7 @@ def context_module(x, filters):
     # First convolution block
     x = convolution_block(x, filters)
     # Dropout layer
-    x = SpatialDropout3D(0.5)(x) 
+    x = SpatialDropout3D(0.65)(x) 
     # Second convolution block
     x = convolution_block(x, filters)
     return x
