135dac76fa178fb9cfe3a5c7399229a9cda55277
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-06-24 15:41:27 +0800

    Update cd.py

--- cd.py
+++ cd.py
@@ -9,7 +9,7 @@ image_pet,label,masker=loading_mask('cd','PET')
 
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, label, 'MRI', 'cd')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'MRI', 'cd')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
@@ -17,13 +17,13 @@ print(f"The function took {elapsed_time:.2f} seconds to run.")
 
 
 
-start_time = time.time()  # Capture start time
+#start_time = time.time()  # Capture start time
 
-performance_dict_pet,all_y_test_pet, all_y_prob_pet, all_predictions_pet=nested_crossvalidation_multi_kernel(image_pet, label, 'PET', 'cd')
-end_time = time.time()  # Capture end time
+#performance_dict_pet,all_y_test_pet, all_y_prob_pet, all_predictions_pet=nested_crossvalidation_multi_kernel(image_pet, label, 'PET', 'cd')
+#end_time = time.time()  # Capture end time
 
-elapsed_time = end_time - start_time  # Calculate elapsed time
-print(f"The function took {elapsed_time:.2f} seconds to run.")
+#elapsed_time = end_time - start_time  # Calculate elapsed time
+#print(f"The function took {elapsed_time:.2f} seconds to run.")
 
 
 
