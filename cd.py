1fd64e80a1962e25c13fe06c0c92880d6313974b
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-07-03 00:46:35 +0800

    Update cd.py

--- cd.py
+++ cd.py
@@ -4,32 +4,32 @@ from main import nested_crossvalidation_multi_kernel, nested_crossvalidation
 
 
 
-image_mri,label,masker=loading_mask('cd','MRI')
-image_pet,label,masker=loading_mask('cd','PET')
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cd')
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'PET', 'cd')
+#image_mri,label,masker=loading_mask('cd','MRI')
+#image_pet,label,masker=loading_mask('cd','PET')
+#performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cd')
+#performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'PET', 'cd')
 image_mri,label,masker=loading_mask('cm','MRI')
 image_pet,label,masker=loading_mask('cm','PET')
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cd')
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'PET', 'cd')
+#performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cd')
+#performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'PET', 'cd')
 
-image_mri,label,masker=loading_mask('dm','MRI')
-image_pet,label,masker=loading_mask('dm','PET')
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cd')
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'PET', 'cd')
+#image_mri,label,masker=loading_mask('dm','MRI')
+#image_pet,label,masker=loading_mask('dm','PET')
+#performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cd')
+#performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'PET', 'cd')
 
-image_mri,label,masker=loading_mask('pc','MRI')
-image_pet,label,masker=loading_mask('pc','PET')
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cd')
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'PET', 'cd')
+#image_mri,label,masker=loading_mask('pc','MRI')
+#image_pet,label,masker=loading_mask('pc','PET')
+#performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cd')
+#performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'PET', 'cd')
 
 
-#start_time = time.time()  # Capture start time
+start_time = time.time()  # Capture start time
 
-#performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'MRI', 'cd')
-#end_time = time.time()  # Capture end time
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'MRI', 'cd')
+end_time = time.time()  # Capture end time
 
-#elapsed_time = end_time - start_time  # Calculate elapsed time
+elapsed_time = end_time - start_time  # Calculate elapsed time
 print(f"The function took {elapsed_time:.2f} seconds to run.")
 
 
