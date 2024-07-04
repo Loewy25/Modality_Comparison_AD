592af8bad743b605a902814efcb1cdcd9590da7f
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-07-05 00:44:29 +0800

    Update cd.py

--- cd.py
+++ cd.py
@@ -8,54 +8,89 @@ from main import nested_crossvalidation_multi_kernel, nested_crossvalidation
 image_mri,label,masker=loading_mask('cd','MRI')
 image_pet,label,masker=loading_mask('cd','PET')
 
-
-
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'multi_kernel', 'cd')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cd')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
 print(f"The function took {elapsed_time:.2f} seconds to run.")
+
+start_time = time.time()  # Capture start time
+
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'MRI', 'cd')
+end_time = time.time()  # Capture end time
+
+elapsed_time = end_time - start_time  # Calculate elapsed time
+print(f"The function took {elapsed_time:.2f} seconds to run.")
+
+
 
 image_mri,label,masker=loading_mask('cm','MRI')
 image_pet,label,masker=loading_mask('cm','PET')
 
-
-
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'multi_kernel', 'cm')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cm')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
 print(f"The function took {elapsed_time:.2f} seconds to run.")
+
+start_time = time.time()  # Capture start time
+
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'MRI', 'cm')
+end_time = time.time()  # Capture end time
+
+elapsed_time = end_time - start_time  # Calculate elapsed time
+print(f"The function took {elapsed_time:.2f} seconds to run.")
+
 
 image_mri,label,masker=loading_mask('dm','MRI')
 image_pet,label,masker=loading_mask('dm','PET')
 
-
-
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'multi_kernel', 'dm')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'dm')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
 print(f"The function took {elapsed_time:.2f} seconds to run.")
+
+start_time = time.time()  # Capture start time
+
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'MRI', 'dm')
+end_time = time.time()  # Capture end time
+
+elapsed_time = end_time - start_time  # Calculate elapsed time
+print(f"The function took {elapsed_time:.2f} seconds to run.")
+
 
 image_mri,label,masker=loading_mask('pc','MRI')
 image_pet,label,masker=loading_mask('pc','PET')
 
-
-
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'multi_kernel', 'pc')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'pc')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
 print(f"The function took {elapsed_time:.2f} seconds to run.")
+
+start_time = time.time()  # Capture start time
+
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_mri, label, 'MRI', 'pc')
+end_time = time.time()  # Capture end time
+
+elapsed_time = end_time - start_time  # Calculate elapsed time
+print(f"The function took {elapsed_time:.2f} seconds to run.")
+#start_time = time.time()  # Capture start time
+
+#performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'multi_kernel', 'cd')
+#end_time = time.time()  # Capture end time
+
+#elapsed_time = end_time - start_time  # Calculate elapsed time
+#print(f"The function took {elapsed_time:.2f} seconds to run.")
 
 
 
@@ -71,10 +106,4 @@ print(f"The function took {elapsed_time:.2f} seconds to run.")
 
 
 
-#start_time = time.time()  # Capture start time
 
-#performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET', 'cd')
-#end_time = time.time()  # Capture end time
-
-#elapsed_time = end_time - start_time  # Calculate elapsed time
-#print(f"The function took {elapsed_time:.2f} seconds to run.")
