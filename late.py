a76c0f043f63d51e93d33913deb4b8fc8b29a9b5
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-07-06 19:54:50 +0800

    Update and rename dm.py to late.py

---
+++ late.py
@@ -0,0 +1,62 @@
+
+import time
+from data_loading import loading_mask
+from main import nested_crossvalidation_multi_kernel, nested_crossvalidation
+
+
+
+
+image_mri,label,masker=loading_mask('cd','MRI')
+image_pet,label,masker=loading_mask('cd','PET')
+
+
+start_time = time.time()  # Capture start time
+
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late', 'cd')
+end_time = time.time()  # Capture end time
+
+elapsed_time = end_time - start_time  # Calculate elapsed time
+print(f"The function took {elapsed_time:.2f} seconds to run.")
+
+image_mri,label,masker=loading_mask('cm','MRI')
+image_pet,label,masker=loading_mask('cm','PET')
+
+
+start_time = time.time()  # Capture start time
+
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late', 'cm')
+end_time = time.time()  # Capture end time
+
+elapsed_time = end_time - start_time  # Calculate elapsed time
+print(f"The function took {elapsed_time:.2f} seconds to run.")
+
+image_mri,label,masker=loading_mask('dm','MRI')
+image_pet,label,masker=loading_mask('dm','PET')
+
+
+start_time = time.time()  # Capture start time
+
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late', 'dm')
+end_time = time.time()  # Capture end time
+
+elapsed_time = end_time - start_time  # Calculate elapsed time
+print(f"The function took {elapsed_time:.2f} seconds to run.")
+
+image_mri,label,masker=loading_mask('pc','MRI')
+image_pet,label,masker=loading_mask('pc','PET')
+
+
+start_time = time.time()  # Capture start time
+
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late', 'pc')
+end_time = time.time()  # Capture end time
+
+elapsed_time = end_time - start_time  # Calculate elapsed time
+print(f"The function took {elapsed_time:.2f} seconds to run.")
+
+
+
+
+
+
+
