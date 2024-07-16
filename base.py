fbfec223b0bbe01d8cdbb9b999433b279b9e6d9b
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-07-16 19:36:50 +0800

    Update base.py

--- base.py
+++ base.py
@@ -5,12 +5,12 @@ from main import nested_crossvalidation_multi_kernel, nested_crossvalidation
 
 
 
-image_mri,label,masker=loading_mask('cd','MRI')
-image_pet,label,masker=loading_mask('cd','PET')
+image_mri,label,masker=loading_mask('cm','MRI')
+image_pet,label,masker=loading_mask('cm','PET')
 
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET_old', 'cd')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation(image_pet, label, 'PET_old', 'cm')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
