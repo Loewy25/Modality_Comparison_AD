c401f7c712de25129b7432cc522c2381ae378540
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-07-17 16:22:42 +0800

    Update multi-kernel.py

--- multi-kernel.py
+++ multi-kernel.py
@@ -29,29 +29,29 @@ end_time = time.time()  # Capture end time
 elapsed_time = end_time - start_time  # Calculate elapsed time
 print(f"The function took {elapsed_time:.2f} seconds to run.")
 
-image_mri,label,masker=loading_mask('dm','MRI')
-image_pet,label,masker=loading_mask('dm','PET')
+# image_mri,label,masker=loading_mask('dm','MRI')
+# image_pet,label,masker=loading_mask('dm','PET')
 
 
-start_time = time.time()  # Capture start time
+# start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'multi_kernel_old', 'dm')
-end_time = time.time()  # Capture end time
+# performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'multi_kernel_old', 'dm')
+# end_time = time.time()  # Capture end time
 
-elapsed_time = end_time - start_time  # Calculate elapsed time
-print(f"The function took {elapsed_time:.2f} seconds to run.")
+# elapsed_time = end_time - start_time  # Calculate elapsed time
+# print(f"The function took {elapsed_time:.2f} seconds to run.")
 
-image_mri,label,masker=loading_mask('pc','MRI')
-image_pet,label,masker=loading_mask('pc','PET')
+# image_mri,label,masker=loading_mask('pc','MRI')
+# image_pet,label,masker=loading_mask('pc','PET')
 
 
-start_time = time.time()  # Capture start time
+# start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'multi_kernel_old', 'pc')
-end_time = time.time()  # Capture end time
+# performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_multi_kernel(image_mri, image_pet, label, 'multi_kernel_old', 'pc')
+# end_time = time.time()  # Capture end time
 
-elapsed_time = end_time - start_time  # Calculate elapsed time
-print(f"The function took {elapsed_time:.2f} seconds to run.")
+# elapsed_time = end_time - start_time  # Calculate elapsed time
+# print(f"The function took {elapsed_time:.2f} seconds to run.")
 
 
 
