2082d63f906c5b2cd6432c5247e1d67cf79eae74
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-07-22 01:20:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -176,8 +176,8 @@ def nested_crossvalidation(data, label, method, task):
             y_train, y_test = np.array(train_label)[train_ix], np.array(train_label)[test_ix]
             # Normalize the training data based on control indices within this fold
             control_indices_train = [i for i, label in enumerate(y_train) if label == 0]
-            X_train, scaler = normalize_features_z(X_train, control_indices_train, return_params=True)
-            X_test = apply_normalization_z(X_test, scaler)
+            X_train, scaler = normalize_features(X_train, control_indices_train, return_params=True)
+            X_test = apply_normalization(X_test, scaler)
             K_train = compute_kernel_matrix(X_train, X_train, linear_kernel)
             K_test = compute_kernel_matrix(X_test, X_train, linear_kernel)
             cv_inner = StratifiedKFold(n_splits=3, shuffle=True, random_state=1)
