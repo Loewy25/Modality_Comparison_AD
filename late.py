b0290cee6ce39d7fd609b4d900fdbe392dfb77d1
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-07-25 00:34:57 +0800

    Update late.py

--- late.py
+++ late.py
@@ -12,7 +12,7 @@ image_pet,label,masker=loading_mask('cd','PET')
 
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late_old', 'cd')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late_WITHOUT', 'cd')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
@@ -24,7 +24,7 @@ image_pet,label,masker=loading_mask('cm','PET')
 
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late_old', 'cm')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late_WITHOUT', 'cm')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
@@ -36,7 +36,7 @@ image_pet,label,masker=loading_mask('dm','PET')
 
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late_old', 'dm')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late_WITHOUT', 'dm')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
@@ -48,7 +48,7 @@ image_pet,label,masker=loading_mask('pc','PET')
 
 start_time = time.time()  # Capture start time
 
-performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late_old', 'pc')
+performance_dict_mri,all_y_test_mri, all_y_prob_mri, all_predictions_mri=nested_crossvalidation_late_fusion(image_mri, image_pet, label, 'Late_WITHOUT', 'pc')
 end_time = time.time()  # Capture end time
 
 elapsed_time = end_time - start_time  # Calculate elapsed time
