f0cef3505e40cec964438deeb922f5febe53d88a
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-09-07 21:53:18 -0500

    Update deep_learning.py

--- deep_learning.py
+++ deep_learning.py
@@ -197,6 +197,7 @@ def pad_image_to_shape(image, target_shape=(128, 128, 128)):
     Assumes that the input is 3D (without a channel dimension).
     """
     current_shape = image.shape  # Get the shape of the 3D image
+    print(current_shape)
 
     # Calculate the padding needed for each dimension (height, width, depth)
     padding = [(max((target_shape[i] - current_shape[i]) // 2, 0),
