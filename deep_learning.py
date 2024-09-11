da8d8db3f9bd8317cb1b0f631f14ae714c000a5b
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-09-11 08:30:51 -0500

    Update deep_learning.py

--- deep_learning.py
+++ deep_learning.py
@@ -64,6 +64,12 @@ def create_3d_cnn(input_shape=(128, 128, 128, 1), num_classes=2):
     conv4_out = x
     x = context_module(x, 128)
     x = Add()([x, conv4_out])
+
+    # Conv4 block (128 filters, stride 2)
+    x = convolution_block(x, 256, strides=(2, 2, 2))
+    conv5_out = x
+    x = context_module(x, 256)
+    x = Add()([x, conv5_out])
     
     # Global Average Pooling
     x = GlobalAveragePooling3D()(x)
