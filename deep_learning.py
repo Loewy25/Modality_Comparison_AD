1fb2c768b86245a91921bc88267b981765e8ced6
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-09-15 19:53:00 -0500

    Update deep_learning.py

--- deep_learning.py
+++ deep_learning.py
@@ -280,7 +280,7 @@ def train_model(X, Y, task, modality, info):
 
         history = model.fit(X_train, Y_train,
                             batch_size=5,
-                            epochs=100,  # Increased epochs
+                            epochs=3,  # Increased epochs
                             validation_data=(X_val, Y_val),
                             callbacks=[early_stopping, reduce_lr])
         histories.append(history)
