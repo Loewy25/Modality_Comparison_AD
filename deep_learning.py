b3780bd1dd4fbf074d678693e80f19c590ef4671
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-09-18 11:02:13 -0500

    Update deep_learning.py

--- deep_learning.py
+++ deep_learning.py
@@ -393,7 +393,7 @@ def loading_mask_3d(task, modality):
 # Main execution
 if __name__ == '__main__':
     task = 'dm'  # Update as per your task
-    modality = 'PET'  # 'MRI' or 'PET'
+    modality = 'MRI'  # 'MRI' or 'PET'
     info = '5_context_from_16_0.4_dropout_1e3_with_augmentation'  # Additional info for saving results
 
     # Load your data
