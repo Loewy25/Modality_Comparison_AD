77cc61edb2af0bafe090d9f3d002d5d031a42e5e
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-09-20 09:21:55 -0500

    Update Transformer.py

--- Transformer.py
+++ Transformer.py
@@ -221,7 +221,7 @@ def loading_mask_3d(task, modality):
 
 # Main execution
 if __name__ == '__main__':
-    task = 'cm'  # Update as per your task
+    task = 'cd'  # Update as per your task
     modality = 'MRI'  # 'MRI' or 'PET'
     info = 'transformer_model_v1'
 
