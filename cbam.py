a601759264b9cea16490917739c0f0752a2f9631
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-10-07 21:55:29 -0500

    Create cbam.py

---
+++ cbam.py
@@ -0,0 +1,459 @@
+import os
+import numpy as np
+import nibabel as nib
+import matplotlib.pyplot as plt
+from nilearn import plotting
+from scipy.stats import zscore
+from scipy.ndimage import zoom, rotate
+
+# Import your own data loading functions
+from data_loading import generate_data_path_less, generate, binarylabel
+
+# Import TensorFlow and Keras
+import tensorflow as tf
+from tensorflow.keras.layers import (Conv3D, Input, LeakyReLU, Add,
+                                     GlobalAveragePooling3D, Dense, Dropout,
+                                     SpatialDropout3D, BatchNormalization)
+from tensorflow.keras.models import Model
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
+# Import Ray and Ray Tune
+import ray
+from ray import tune
+from ray.tune.schedulers import HyperBandScheduler
+from ray.train.tensorflow.keras import ReportCheckpointCallback
+
+# Initialize Ray
+ray.init(ignore_reinit_error=True)
+
+# -------------------------------
+# Utility Classes
+# -------------------------------
+
+class Utils:
+    """Utility functions for directory management and image resizing."""
+
+    @staticmethod
+    def ensure_directory_exists(directory):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    @staticmethod
+    def resize_image(image, target_shape):
+        if len(image.shape) == 4:
+            # Assuming the last dimension is the channel dimension
+            spatial_dims = image.shape[:3]
+            zoom_factors = [target_shape[i] / spatial_dims[i] for i in range(3)] + [1]
+        elif len(image.shape) == 3:
+            zoom_factors = [target_shape[i] / image.shape[i] for i in range(3)]
+        else:
+            raise ValueError(f"Unexpected image shape: {image.shape}. Expected 3D or 4D image.")
+
+        # Apply zoom to the image
+        resized_image = zoom(image, zoom_factors, order=1)
+        return resized_image
+
+class CBAM3D(tf.keras.layers.Layer):
+    """Convolutional Block Attention Module for 3D CNNs."""
+
+    def __init__(self, channels, reduction_ratio=16, kernel_size=7, **kwargs):
+        super(CBAM3D, self).__init__(**kwargs)
+        self.channels = channels
+        self.reduction_ratio = reduction_ratio
+        self.kernel_size = kernel_size
+
+        # Channel Attention components
+        self.global_avg_pool = GlobalAveragePooling3D()
+        self.global_max_pool = GlobalMaxPooling3D()
+        self.shared_dense1 = Dense(self.channels // self.reduction_ratio,
+                                   activation='relu',
+                                   kernel_initializer='he_normal',
+                                   use_bias=True,
+                                   bias_initializer='zeros')
+        self.shared_dense2 = Dense(self.channels,
+                                   activation='sigmoid',
+                                   kernel_initializer='he_normal',
+                                   use_bias=True,
+                                   bias_initializer='zeros')
+
+        # Spatial Attention components
+        self.conv_spatial = Conv3D(filters=1,
+                                   kernel_size=(self.kernel_size, self.kernel_size, self.kernel_size),
+                                   strides=(1, 1, 1),
+                                   padding='same',
+                                   activation='sigmoid',
+                                   kernel_initializer='he_normal',
+                                   use_bias=False)
+
+    def call(self, inputs):
+        # ----- Channel Attention Module -----
+        # Feature descriptor on the channel axis
+        avg_pool = self.global_avg_pool(inputs)  # Shape: (batch, channels)
+        avg_pool = self.shared_dense1(avg_pool)
+        avg_pool = self.shared_dense2(avg_pool)  # Shape: (batch, channels)
+
+        max_pool = self.global_max_pool(inputs)  # Shape: (batch, channels)
+        max_pool = self.shared_dense1(max_pool)
+        max_pool = self.shared_dense2(max_pool)  # Shape: (batch, channels)
+
+        # Combine and apply sigmoid activation
+        channel_attention = Add()([avg_pool, max_pool])  # Shape: (batch, channels)
+        channel_attention = Reshape((1, 1, 1, self.channels))(channel_attention)  # Shape: (batch, 1, 1, 1, channels)
+        channel_refined = Multiply()([inputs, channel_attention])  # Shape: (batch, D, H, W, channels)
+
+        # ----- Spatial Attention Module -----
+        # Feature descriptor on the spatial axis
+        avg_spatial = tf.reduce_mean(channel_refined, axis=-1, keepdims=True)  # Shape: (batch, D, H, W, 1)
+        max_spatial = tf.reduce_max(channel_refined, axis=-1, keepdims=True)   # Shape: (batch, D, H, W, 1)
+        concat = Concatenate(axis=-1)([avg_spatial, max_spatial])              # Shape: (batch, D, H, W, 2)
+
+        # Apply convolution to get spatial attention map
+        spatial_attention = self.conv_spatial(concat)                         # Shape: (batch, D, H, W, 1)
+        spatial_refined = Multiply()([channel_refined, spatial_attention])    # Shape: (batch, D, H, W, channels)
+
+        return spatial_refined
+
+    def get_config(self):
+        config = super(CBAM3D, self).get_config()
+        config.update({
+            'channels': self.channels,
+            'reduction_ratio': self.reduction_ratio,
+            'kernel_size': self.kernel_size
+        })
+        return config
+
+class CNNModel:
+    """Class to create and manage the 3D CNN model with CBAM attention mechanisms."""
+
+    @staticmethod
+    def convolution_block(x, filters, kernel_size=(3, 3, 3), strides=(1, 1, 1), l2_reg=1e-5):
+        x = Conv3D(filters, kernel_size, strides=strides, padding='same',
+                   kernel_regularizer=l2(l2_reg))(x)
+        x = BatchNormalization()(x)
+        x = LeakyReLU()(x)
+        return x
+
+    @staticmethod
+    def context_module(x, filters, dropout_rate=0.05, l2_reg=1e-5):
+        x = CNNModel.convolution_block(x, filters, l2_reg=l2_reg)
+        x = SpatialDropout3D(dropout_rate)(x)
+        x = CNNModel.convolution_block(x, filters, l2_reg=l2_reg)
+        return x
+
+    @staticmethod
+    def create_model(input_shape=(128, 128, 128, 1), num_classes=2, dropout_rate=0.05, l2_reg=1e-5, use_cbam=True):
+        input_img = Input(shape=input_shape)
+
+        # Conv1 block (16 filters)
+        x = CNNModel.convolution_block(input_img, 16, l2_reg=l2_reg)
+        conv1_out = x
+        if use_cbam:
+            x = CBAM3D(channels=16)(x)
+        x = CNNModel.context_module(x, 16, dropout_rate=dropout_rate, l2_reg=l2_reg)
+        x = Add()([x, conv1_out])
+
+        # Conv2 block (32 filters, stride 2)
+        x = CNNModel.convolution_block(x, 32, strides=(2, 2, 2), l2_reg=l2_reg)
+        conv2_out = x
+        if use_cbam:
+            x = CBAM3D(channels=32)(x)
+        x = CNNModel.context_module(x, 32, dropout_rate=dropout_rate, l2_reg=l2_reg)
+        x = Add()([x, conv2_out])
+
+        # Conv3 block (64 filters, stride 2)
+        x = CNNModel.convolution_block(x, 64, strides=(2, 2, 2), l2_reg=l2_reg)
+        conv3_out = x
+        if use_cbam:
+            x = CBAM3D(channels=64)(x)
+        x = CNNModel.context_module(x, 64, dropout_rate=dropout_rate, l2_reg=l2_reg)
+        x = Add()([x, conv3_out])
+
+        # Conv4 block (128 filters, stride 2)
+        x = CNNModel.convolution_block(x, 128, strides=(2, 2, 2), l2_reg=l2_reg)
+        conv4_out = x
+        if use_cbam:
+            x = CBAM3D(channels=128)(x)
+        x = CNNModel.context_module(x, 128, dropout_rate=dropout_rate, l2_reg=l2_reg)
+        x = Add()([x, conv4_out])
+
+        # Conv5 block (256 filters, stride 2)
+        x = CNNModel.convolution_block(x, 256, strides=(2, 2, 2), l2_reg=l2_reg)
+        conv5_out = x
+        if use_cbam:
+            x = CBAM3D(channels=256)(x)
+        x = CNNModel.context_module(x, 256, dropout_rate=dropout_rate, l2_reg=l2_reg)
+        x = Add()([x, conv5_out])
+
+        # Global Average Pooling
+        x = GlobalAveragePooling3D()(x)
+
+        # Dropout for regularization
+        x = Dropout(dropout_rate)(x)
+
+        # Dense layer with softmax for classification
+        output = Dense(num_classes, activation='softmax')(x)
+
+        model = Model(inputs=input_img, outputs=output)
+        model.summary()
+
+        return model
+
+class DataLoader:
+    """Class to handle data loading and preprocessing."""
+
+    @staticmethod
+    def loading_mask_3d(task, modality):
+        images_pet, images_mri, labels = generate_data_path_less()
+        original_imgs = []  # Initialize the list to store original images
+
+        if modality == 'PET':
+            data_train, train_label = generate(images_pet, labels, task)
+        elif modality == 'MRI':
+            data_train, train_label = generate(images_mri, labels, task)
+        else:
+            raise ValueError(f"Unsupported modality: {modality}")
+
+        train_data = []
+        target_shape = (128, 128, 128)
+
+        for i in range(len(data_train)):
+            nifti_img = nib.load(data_train[i])
+            original_imgs.append(nifti_img)  # Store the original NIfTI image
+
+            reshaped_data = nifti_img.get_fdata()
+            reshaped_data = zscore(reshaped_data, axis=None)
+
+            # Resize the image to (128, 128, 128)
+            resized_data = Utils.resize_image(reshaped_data, target_shape)
+
+            train_data.append(resized_data)
+
+        train_label = binarylabel(train_label, task)
+        train_data = np.array(train_data)
+
+        return train_data, train_label, original_imgs
+
+    @staticmethod
+    def augment_data(X, flip_prob=0.1, rotate_prob=0.1):
+        augmented_X = []
+        for img in X:
+            img_aug = img.copy()
+            # Randomly flip along each axis with specified probability
+            if np.random.rand() < flip_prob:
+                img_aug = np.flip(img_aug, axis=0)  # Flip along x-axis
+            if np.random.rand() < flip_prob:
+                img_aug = np.flip(img_aug, axis=1)  # Flip along y-axis
+            if np.random.rand() < flip_prob:
+                img_aug = np.flip(img_aug, axis=2)  # Flip along z-axis
+
+            # Random rotation with specified probability
+            if np.random.rand() < rotate_prob:
+                angle = np.random.uniform(-10, 10)  # Rotate between -10 and 10 degrees
+                img_aug = rotate(img_aug, angle, axes=(0, 1), reshape=False, order=1)  # Rotate in x-y plane
+
+            augmented_X.append(img_aug)
+        return np.array(augmented_X)
+
+# -------------------------------
+# Trainer Class
+# -------------------------------
+
+class Trainer:
+    """Class to handle model training and hyperparameter tuning."""
+
+    @staticmethod
+    def train_model(config, X_train, Y_train, X_val, Y_val):
+        # Unpack hyperparameters from the config dictionary
+        learning_rate = config["learning_rate"]
+        batch_size = config["batch_size"]
+        dropout_rate = config["dropout_rate"]
+        l2_reg = config["l2_reg"]
+        flip_prob = config["flip_prob"]
+        rotate_prob = config["rotate_prob"]
+
+        # Apply data augmentation with specified probabilities
+        X_train_augmented = DataLoader.augment_data(X_train, flip_prob=flip_prob, rotate_prob=rotate_prob)
+
+        # Create model with hyperparameters
+        model = CNNModel.create_model(
+            input_shape=(128, 128, 128, 1),
+            num_classes=2,
+            dropout_rate=dropout_rate,
+            l2_reg=l2_reg,
+            use_cbam=True  # CBAM is integral and always used
+        )
+        model.compile(
+            optimizer=Adam(learning_rate=learning_rate),
+            loss='categorical_crossentropy',
+            metrics=['accuracy', tf.keras.metrics.AUC(name='auc')]
+        )
+
+        # Define callbacks
+        early_stopping = EarlyStopping(
+            monitor='val_loss',
+            patience=20,
+            mode='min',
+            verbose=0,
+            restore_best_weights=True
+        )
+
+        reduce_lr = ReduceLROnPlateau(
+            monitor='val_loss',
+            factor=0.5,
+            patience=5,
+            mode='min',
+            verbose=0
+        )
+
+        # Use ReportCheckpointCallback
+        report_checkpoint_callback = ReportCheckpointCallback()
+
+        # Train the model
+        model.fit(
+            X_train_augmented, Y_train,
+            validation_data=(X_val, Y_val),
+            epochs=100,  # Set a high epoch count; early stopping will handle termination
+            batch_size=batch_size,
+            callbacks=[
+                early_stopping,
+                reduce_lr,
+                report_checkpoint_callback
+            ],
+            verbose=0
+        )
+
+        # Report metrics to Ray Tune
+        val_loss, val_accuracy, val_auc = model.evaluate(X_val, Y_val, verbose=0)
+        tune.report(val_loss=val_loss, val_accuracy=val_accuracy, val_auc=val_auc)
+
+        # After training, clear the session and collect garbage to free memory
+        tf.keras.backend.clear_session()
+        import gc
+        gc.collect()
+
+    @staticmethod
+    def tune_model(X, Y, task, modality, info):
+        # Split data using StratifiedKFold
+        stratified_kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+        for train_idx, val_idx in stratified_kfold.split(X, Y.argmax(axis=1)):
+            X_train, X_val = X[train_idx], X[val_idx]
+            Y_train, Y_val = Y[train_idx], Y[val_idx]
+            break  # Use the first split
+
+        # Define search space excluding CBAM as it's integral
+        config = {
+            "learning_rate": tune.loguniform(1e-4, 1e-2),
+            "batch_size": tune.choice([4, 8, 16]),
+            "dropout_rate": tune.uniform(0.1, 0.5),
+            "l2_reg": tune.loguniform(1e-5, 1e-3),
+            "flip_prob": tune.uniform(0.0, 0.3),
+            "rotate_prob": tune.uniform(0.0, 0.3),
+        }
+
+        # Scheduler for early stopping bad trials
+        scheduler = HyperBandScheduler(
+            time_attr="training_iteration",
+            max_t=100,
+            grace_period=10,
+            reduction_factor=3
+        )
+
+        # Execute tuning
+        analysis = tune.run(
+            tune.with_parameters(Trainer.train_model, X_train=X_train, Y_train=Y_train, X_val=X_val, Y_val=Y_val),
+            resources_per_trial={"cpu": 2, "gpu": 1},  # Adjust based on your hardware
+            config=config,
+            metric="val_auc",  # Use AUC for selecting the best model
+            mode="max",
+            num_samples=20,  # Increased samples for better exploration
+            scheduler=scheduler,
+            name="hyperparameter_tuning",
+            max_concurrent_trials=4  # Adjust based on available GPUs
+        )
+
+        # Get the best trial
+        best_trial = analysis.get_best_trial("val_auc", "max", "last")
+        print("Best trial config: {}".format(best_trial.config))
+        print("Best trial final validation AUC: {:.4f}".format(best_trial.last_result["val_auc"]))
+
+        # Train the best model on the full training data
+        best_model = CNNModel.create_model(
+            input_shape=(128, 128, 128, 1),
+            num_classes=2,
+            dropout_rate=best_trial.config["dropout_rate"],
+            l2_reg=best_trial.config["l2_reg"],
+            use_cbam=True  # CBAM is integral
+        )
+        best_model.compile(
+            optimizer=Adam(learning_rate=best_trial.config["learning_rate"]),
+            loss='categorical_crossentropy',
+            metrics=['accuracy', tf.keras.metrics.AUC(name='auc')]
+        )
+
+        # Apply data augmentation with best probabilities
+        X_train_augmented = DataLoader.augment_data(
+            X_train,
+            flip_prob=best_trial.config["flip_prob"],
+            rotate_prob=best_trial.config["rotate_prob"]
+        )
+
+        # Define callbacks
+        early_stopping = EarlyStopping(
+            monitor='val_loss',
+            patience=20,
+            mode='min',
+            verbose=1,
+            restore_best_weights=True
+        )
+
+        reduce_lr = ReduceLROnPlateau(
+            monitor='val_loss',
+            factor=0.5,
+            patience=5,
+            mode='min',
+            verbose=1
+        )
+
+        # Retrain on the full data
+        history = best_model.fit(
+            X_train_augmented, Y_train,
+            validation_data=(X_val, Y_val),
+            epochs=200,  # Set a high epoch count; early stopping will handle termination
+            batch_size=best_trial.config["batch_size"],
+            callbacks=[early_stopping, reduce_lr],
+            verbose=1
+        )
+
+        # Evaluate on validation set
+        y_val_pred = best_model.predict(X_val)
+        final_auc = roc_auc_score(Y_val[:, 1], y_val_pred[:, 1])
+        print(f'Final AUC on validation data: {final_auc:.4f}')
+
+        return best_model
+
+# -------------------------------
+# Main Execution Flow
+# -------------------------------
+
+def main():
+    task = 'cd'  # Update as per your task
+    modality = 'MRI'  # 'MRI' or 'PET'
+    info = 'test'  # Additional info for saving results
+
+    # Load your data
+    train_data, train_label, original_imgs = DataLoader.loading_mask_3d(task, modality)
+    X = np.array(train_data)
+    X = np.expand_dims(X, axis=-1)  # Add channel dimension if not already present
+    Y = to_categorical(train_label, num_classes=2)
+
+    # Train the model with hyperparameter tuning and get the best trained model
+    best_model = Trainer.tune_model(X, Y, task, modality, info)
+
+    # Shutdown Ray
+    ray.shutdown()
+
+if __name__ == '__main__':
+    main()
