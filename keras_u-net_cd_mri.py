f55bac9315fcaf53800b8f634c1b06f3fb6c73e4
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-10-14 18:18:54 -0500

    Update keras_u-net_cd_mri.py

--- keras_u-net_cd_mri.py
+++ keras_u-net_cd_mri.py
@@ -306,7 +306,7 @@ class Trainer:
         return model
 
     @staticmethod
-    def tune_model_nested_cv(X, Y, task, modality, info, n_splits=2, max_trials=10, executions_per_trial=1):
+    def tune_model_nested_cv(X, Y, task, modality, info, n_splits=2, max_trials=2, executions_per_trial=1):
         """Performs hyperparameter tuning using nested cross-validation."""
         # Define the cross-validation strategy
         stratified_kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=2)
@@ -360,7 +360,7 @@ class Trainer:
             tuner.search(
                 X_train_augmented, Y_train,
                 validation_data=(X_val, Y_val),
-                epochs=80,  # Set a high number; early stopping will handle it
+                epochs=3,  # Set a high number; early stopping will handle it
                 batch_size=5,  # Temporary batch size; will adjust based on hyperparameter
                 callbacks=callbacks,
                 verbose=1,
@@ -413,7 +413,7 @@ class Trainer:
             history = final_model.fit(
                 X_train_augmented, Y_train,
                 validation_data=(X_val, Y_val),
-                epochs=250,  # Adjust as needed
+                epochs=3,  # Adjust as needed
                 batch_size=5,
                 callbacks=final_callbacks,
                 verbose=1
