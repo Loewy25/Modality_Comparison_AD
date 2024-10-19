b91a62039926b4595626b405a01175a1f91bae8d
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-10-19 18:05:13 -0500

    Update ViT_cd_mri.py

--- ViT_cd_mri.py
+++ ViT_cd_mri.py
@@ -1,18 +1,14 @@
 import os
 import numpy as np
 import nibabel as nib
-import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.layers import (Input, Dense, Conv3D, Dropout, LayerNormalization,
-                                     Add, Reshape, MultiHeadAttention)
+from tensorflow.keras.layers import (Input, Dense, LayerNormalization, Dropout, Add, Reshape)
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from tensorflow.keras.metrics import AUC
 from tensorflow.keras.utils import to_categorical
-from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import StratifiedKFold
-from nilearn import plotting
 from scipy.stats import zscore
 from scipy.ndimage import zoom, rotate
 import keras_tuner as kt
@@ -31,16 +27,7 @@ class Utils:
 
     @staticmethod
     def resize_image(image, target_shape):
-        if len(image.shape) == 4:
-            # Assuming the last dimension is the channel dimension
-            spatial_dims = image.shape[:3]
-            zoom_factors = [target_shape[i] / spatial_dims[i] for i in range(3)] + [1]
-        elif len(image.shape) == 3:
-            zoom_factors = [target_shape[i] / image.shape[i] for i in range(3)]
-        else:
-            raise ValueError(f"Unexpected image shape: {image.shape}. Expected 3D or 4D image.")
-
-        # Apply zoom to the image
+        zoom_factors = [target_shape[i] / image.shape[i] for i in range(3)]
         resized_image = zoom(image, zoom_factors, order=1)
         return resized_image
 
@@ -50,7 +37,7 @@ class DataLoader:
     @staticmethod
     def loading_mask_3d(task, modality):
         images_pet, images_mri, labels = generate_data_path_less()
-        original_imgs = []  # Initialize the list to store original images
+        original_imgs = []
 
         if modality == 'PET':
             data_train, train_label = generate(images_pet, labels, task)
@@ -64,14 +51,12 @@ class DataLoader:
 
         for i in range(len(data_train)):
             nifti_img = nib.load(data_train[i])
-            original_imgs.append(nifti_img)  # Store the original NIfTI image
+            original_imgs.append(nifti_img)
 
             reshaped_data = nifti_img.get_fdata()
             reshaped_data = zscore(reshaped_data, axis=None)
 
-            # Resize the image to (128, 128, 128)
             resized_data = Utils.resize_image(reshaped_data, target_shape)
-
             train_data.append(resized_data)
 
         train_label = binarylabel(train_label, task)
@@ -81,95 +66,144 @@ class DataLoader:
 
     @staticmethod
     def augment_data(X, flip_prob=0.1, rotate_prob=0.1):
-        """Applies random flipping and rotation to the data based on given probabilities."""
         augmented_X = []
         for img in X:
             img_aug = img.copy()
-            # Randomly flip along each axis with specified probability
             if np.random.rand() < flip_prob:
-                img_aug = np.flip(img_aug, axis=0)  # Flip along x-axis
+                img_aug = np.flip(img_aug, axis=0)
             if np.random.rand() < flip_prob:
-                img_aug = np.flip(img_aug, axis=1)  # Flip along y-axis
+                img_aug = np.flip(img_aug, axis=1)
             if np.random.rand() < flip_prob:
-                img_aug = np.flip(img_aug, axis=2)  # Flip along z-axis
+                img_aug = np.flip(img_aug, axis=2)
 
-            # Random rotation with specified probability
             if np.random.rand() < rotate_prob:
-                angle = np.random.uniform(-10, 10)  # Rotate between -10 and 10 degrees
+                angle = np.random.uniform(-10, 10)
                 axes = np.random.choice([(0, 1), (0, 2), (1, 2)])
                 img_aug = rotate(img_aug, angle, axes=axes, reshape=False, order=1)
 
             augmented_X.append(img_aug)
         return np.array(augmented_X)
 
-class ViTModel:
-    """Class to create and manage the ViT model for 3D images."""
+class ViTModelBuilder:
+    """Class to build the Vision Transformer model."""
 
-    @staticmethod
-    def transformer_block(x, num_heads, mlp_dim, dropout_rate):
+    def __init__(self, input_shape=(128, 128, 128), num_classes=2, patch_size=(16, 16, 16),
+                 d_model=128, num_heads=8, d_ff=256, num_layers=8, dropout_rate=0.1):
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+        self.patch_size = patch_size
+        self.d_model = d_model  # Embedding dimension
+        self.num_heads = num_heads
+        self.d_ff = d_ff  # Hidden layer size in MLP
+        self.num_layers = num_layers
+        self.dropout_rate = dropout_rate
+
+    # Custom Layer for Class Token
+    class ClassToken(tf.keras.layers.Layer):
+        def build(self, input_shape):
+            self.cls_token = self.add_weight(
+                shape=(1, 1, input_shape[-1]),
+                initializer='random_normal',
+                trainable=True,
+                name='cls_token'
+            )
+            super().build(input_shape)
+
+        def call(self, inputs):
+            batch_size = tf.shape(inputs)[0]
+            cls_tokens = tf.broadcast_to(self.cls_token, [batch_size, 1, inputs.shape[-1]])
+            return tf.concat([cls_tokens, inputs], axis=1)
+
+    # Custom Layer for Positional Embeddings
+    class AddPositionEmbs(tf.keras.layers.Layer):
+        def build(self, input_shape):
+            self.pos_emb = self.add_weight(
+                shape=(1, input_shape[1], input_shape[2]),
+                initializer='random_normal',
+                trainable=True,
+                name='pos_embedding'
+            )
+            super().build(input_shape)
+
+        def call(self, inputs):
+            return inputs + self.pos_emb
+
+    # Transformer block adapted for ViT
+    def transformer_block(self, inputs):
         # Layer normalization
-        x_norm1 = LayerNormalization(epsilon=1e-6)(x)
+        x_norm1 = LayerNormalization(epsilon=1e-6)(inputs)
         # Multi-Head Self-Attention
-        attention_output = MultiHeadAttention(num_heads=num_heads, key_dim=x.shape[-1] // num_heads)(x_norm1, x_norm1)
+        attention_output = tf.keras.layers.MultiHeadAttention(
+            num_heads=self.num_heads, key_dim=self.d_model // self.num_heads
+        )(x_norm1, x_norm1)
         # Dropout
-        attention_output = Dropout(dropout_rate)(attention_output)
+        attention_output = Dropout(self.dropout_rate)(attention_output)
         # Residual connection
-        x1 = Add()([x, attention_output])
+        x1 = Add()([inputs, attention_output])
 
         # Layer normalization
         x_norm2 = LayerNormalization(epsilon=1e-6)(x1)
-        # MLP
-        mlp_output = Dense(mlp_dim, activation='gelu')(x_norm2)
-        mlp_output = Dropout(dropout_rate)(mlp_output)
-        mlp_output = Dense(x.shape[-1])(mlp_output)
-        mlp_output = Dropout(dropout_rate)(mlp_output)
+        # MLP with two layers
+        mlp_output = Dense(self.d_ff, activation='gelu')(x_norm2)
+        mlp_output = Dropout(self.dropout_rate)(mlp_output)
+        mlp_output = Dense(self.d_model)(mlp_output)
+        # Dropout
+        mlp_output = Dropout(self.dropout_rate)(mlp_output)
         # Residual connection
         x2 = Add()([x1, mlp_output])
 
         return x2
 
-    @staticmethod
-    def create_model(input_shape=(128, 128, 128, 1), num_classes=2, patch_size=(16, 16, 16),
-                     num_layers=8, d_model=128, num_heads=8, mlp_dim=512, dropout_rate=0.1):
-        inputs = Input(shape=input_shape)
-
+    # Patch embedding layer
+    def patch_embedding_layer(self):
+        inputs = Input(shape=self.input_shape) 
         # Compute number of patches
-        num_patches = (input_shape[0] // patch_size[0]) * \
-                      (input_shape[1] // patch_size[1]) * \
-                      (input_shape[2] // patch_size[2])
+        num_patches = (self.input_shape[0] // self.patch_size[0]) * \
+                      (self.input_shape[1] // self.patch_size[1]) * \
+                      (self.input_shape[2] // self.patch_size[2])
 
-        # Patch embedding
-        x = Conv3D(filters=d_model, kernel_size=patch_size, strides=patch_size, padding='valid')(inputs)
-        x = Reshape((num_patches, d_model))(x)
-
-        # Class token
-        class_token = tf.Variable(tf.zeros((1, 1, d_model)), trainable=True, name='class_token')
-        batch_size = tf.shape(x)[0]
-        class_tokens = tf.broadcast_to(class_token, [batch_size, 1, d_model])
-        x = tf.concat([class_tokens, x], axis=1)
-
-        # Positional embeddings
-        num_positions = num_patches + 1  # +1 for the class token
-        positional_embedding = tf.Variable(
-            initial_value=tf.random.normal(shape=(1, num_positions, d_model)),
-            trainable=True,
-            name='positional_embedding'
+        # Extract patches and flatten them
+        patches = tf.image.extract_patches(
+            images=inputs,
+            sizes=[1, self.patch_size[0], self.patch_size[1], self.patch_size[2], 1],
+            strides=[1, self.patch_size[0], self.patch_size[1], self.patch_size[2], 1],
+            rates=[1, 1, 1, 1, 1],
+            padding='VALID'
         )
-        x = x + positional_embedding
+        patches_shape = tf.shape(patches)
+        patch_volume = patches_shape[-1]
+        x = tf.reshape(patches, [patches_shape[0], num_patches, patch_volume])
 
-        # Transformer blocks
-        for _ in range(num_layers):
-            x = ViTModel.transformer_block(x, num_heads, mlp_dim, dropout_rate)
+        # Linear projection of flattened patches to embedding dimension
+        x = Dense(self.d_model)(x)
 
-        # Final layer normalization
+        return Model(inputs=inputs, outputs=x, name='patch_embedding')
+
+    # Create the ViT model
+    def create_model(self):
+        inputs = Input(shape=self.input_shape + (1,))
+
+        # Patch Embedding
+        x = self.patch_embedding_layer()(inputs)
+
+        # Add class token and positional embeddings
+        x = self.ClassToken()(x)
+        x = self.AddPositionEmbs()(x)
+
+        # Transformer layers
+        for _ in range(self.num_layers):
+            x = self.transformer_block(x)
+
+        # Layer normalization
         x = LayerNormalization(epsilon=1e-6)(x)
-        # Classification head
-        class_token_output = x[:, 0]  # Extract the class token
-        x = Dense(mlp_dim, activation='gelu')(class_token_output)
-        x = Dropout(dropout_rate)(x)
-        outputs = Dense(num_classes, activation='softmax')(x)
 
-        model = Model(inputs=inputs, outputs=outputs)
+        # Use the class token output for classification
+        class_token_output = x[:, 0]
+
+        # Classification head (single linear layer)
+        output = Dense(self.num_classes, activation='softmax')(class_token_output)
+
+        model = Model(inputs=inputs, outputs=output)
         model.summary()
 
         return model
@@ -181,29 +215,30 @@ class Trainer:
     def build_model(hp):
         """Builds and compiles the model using hyperparameters from Keras Tuner."""
         # Sample hyperparameters
-        learning_rate = hp.Float('learning_rate', 1e-5, 1e-3, sampling='log')
-        dropout_rate = hp.Float('dropout_rate', 0.1, 0.5, step=0.1)
+        dropout_rate = hp.Float('dropout_rate', 0.0, 0.5, step=0.1)
         num_layers = hp.Int('num_layers', min_value=4, max_value=8, step=2)
         d_model = hp.Choice('d_model', [64, 128, 256])
         num_heads = hp.Choice('num_heads', [4, 8])
-        mlp_dim = hp.Choice('mlp_dim', [128, 256, 512])
+        d_ff = hp.Choice('d_ff', [128, 256, 512])
+        patch_size = hp.Choice('patch_size', [(8, 8, 8), (16, 16, 16)])
 
         # Ensure d_model is divisible by num_heads
         if d_model % num_heads != 0:
             d_model = num_heads * (d_model // num_heads)
-        
+
         # Build the model
-        model = ViTModel.create_model(
-            input_shape=(128, 128, 128, 1),
+        vit_builder = ViTModelBuilder(
+            input_shape=(128, 128, 128),
             num_classes=2,
-            patch_size=(16, 16, 16),  # Adjust patch size if needed
-            num_layers=num_layers,
+            patch_size=patch_size,
             d_model=d_model,
             num_heads=num_heads,
-            mlp_dim=mlp_dim,
+            d_ff=d_ff,
+            num_layers=num_layers,
             dropout_rate=dropout_rate
         )
-        
+        model = vit_builder.create_model()
+
         # Compile the model
         model.compile(
             optimizer=Adam(learning_rate=learning_rate),
@@ -215,14 +250,11 @@ class Trainer:
     @staticmethod
     def tune_model_nested_cv(X, Y, task, modality, info, n_splits=3, max_trials=8, executions_per_trial=1):
         """Performs hyperparameter tuning using nested cross-validation."""
-        # Define the cross-validation strategy
         stratified_kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=2)
 
-        # Initialize variables to store results
         fold_results = []
         best_hyperparameters = []
 
-        # Iterate over each fold
         for fold, (train_idx, val_idx) in enumerate(stratified_kfold.split(X, Y.argmax(axis=1)), 1):
             print(f"\nStarting fold {fold}/{n_splits}")
             X_train, X_val = X[train_idx], X[val_idx]
@@ -234,7 +266,7 @@ class Trainer:
             )
             tuner_dir = os.path.join('keras_tuner_dir', task, modality, info, f"fold_{fold}")
             os.makedirs(tuner_dir, exist_ok=True)
-            # Define the search space within the build_model function
+
             tuner = kt.RandomSearch(
                 hypermodel=Trainer.build_model,
                 objective=kt.Objective("val_auc", direction="max"),
@@ -249,7 +281,7 @@ class Trainer:
             callbacks = [
                 EarlyStopping(
                     monitor='val_loss',
-                    patience=20,
+                    patience=50,
                     mode='min',
                     verbose=1,
                     restore_best_weights=True
@@ -257,7 +289,7 @@ class Trainer:
                 ReduceLROnPlateau(
                     monitor='val_loss',
                     factor=0.5,
-                    patience=5,
+                    patience=10,
                     mode='min',
                     verbose=1
                 )
@@ -267,8 +299,8 @@ class Trainer:
             tuner.search(
                 X_train_augmented, Y_train,
                 validation_data=(X_val, Y_val),
-                epochs=100,  # Set a high number; early stopping will handle it
-                batch_size=1,  # Adjust batch size based on memory constraints
+                epochs=80,
+                batch_size=5,
                 callbacks=callbacks,
                 verbose=1,
             )
@@ -282,7 +314,7 @@ class Trainer:
             print(f"Number of Layers: {best_hps.get('num_layers')}")
             print(f"d_model (Embedding Dimension): {best_hps.get('d_model')}")
             print(f"Number of Heads: {best_hps.get('num_heads')}")
-            print(f"MLP Dimension: {best_hps.get('mlp_dim')}")
+            print(f"d_ff (Feed-Forward Dimension): {best_hps.get('d_ff')}")
 
             # Build a new model with the best hyperparameters
             final_model = Trainer.build_model(best_hps)
@@ -291,7 +323,7 @@ class Trainer:
             final_callbacks = [
                 EarlyStopping(
                     monitor='val_loss',
-                    patience=20,
+                    patience=50,
                     mode='min',
                     verbose=1,
                     restore_best_weights=True
@@ -299,7 +331,7 @@ class Trainer:
                 ReduceLROnPlateau(
                     monitor='val_loss',
                     factor=0.5,
-                    patience=5,
+                    patience=10,
                     mode='min',
                     verbose=1
                 )
@@ -309,8 +341,8 @@ class Trainer:
             history = final_model.fit(
                 X_train_augmented, Y_train,
                 validation_data=(X_val, Y_val),
-                epochs=100,  # Adjust as needed
-                batch_size=1,  # Adjust batch size based on memory constraints
+                epochs=250,
+                batch_size=5,
                 callbacks=final_callbacks,
                 verbose=1
             )
@@ -332,10 +364,9 @@ class Trainer:
         return average_auc
 
 def main():
-    # Clear the entire keras_tuner_dir to start fresh
     task = 'cd'  # Update as per your task
     modality = 'MRI'  # 'MRI' or 'PET'
-    info = 'vit_model'  # Additional info for saving results
+    info = 'vit_model_final'  # Additional info for saving results
 
     # Load your data
     train_data, train_label, original_imgs = DataLoader.loading_mask_3d(task, modality)
@@ -354,3 +385,4 @@ if __name__ == '__main__':
     np.random.seed(seed)
     random.seed(seed)
     main()
+
