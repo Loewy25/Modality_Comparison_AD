db3a6e6b8e67bbb714f2dd8f72f0a1124db92e16
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2024-10-22 16:10:43 -0500

    Update ViT_cd_mri.py

--- ViT_cd_mri.py
+++ ViT_cd_mri.py
@@ -348,7 +348,7 @@ class Trainer:
 def main():
     task = 'cd'  # Update as per your task
     modality = 'MRI'  # 'MRI' or 'PET'
-    info = 'vit_model_final2'  # Additional info for saving results
+    info = 'vit_model_final3'  # Additional info for saving results
 
     # Load your data
     train_data, train_label, original_imgs = DataLoader.loading_mask_3d(task, modality)
